73b7ae56b4e3ba1d5a7228c87188d77f924ad8a8
TommyCat <t06t06ca2@gmail.com>
2024-12-28 22:51:31 +0900

    cap2lctrlを追加

---
+++ src/caps2lctrl/main.cpp
@@ -0,0 +1,26 @@
+#include "CregHandler.hpp"
+#include "win_string.hpp"
+
+using namespace CompReg;
+
+int main() {
+    // キーの書込ハンドルを取得する
+    const win32str Path = TEXT("SYSTEM\\CurrentControlSet\\Control\\Keyboard Layout");
+    const auto Handle = CompReg::OpenRegKey(HKEY_LOCAL_MACHINE, Path, KEY_SET_VALUE);
+
+    // ハンドルが有効かを調べる
+    if (!Handle.has_value()) {
+        return Handle.error();
+    }
+    
+    // 値を書き込む
+    const std::vector<uint8_t> bin = {
+        0x00, 0x00, 0x00 ,0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x02, 0x00, 0x00, 0x00,
+        0x1d, 0x00, 0x3a, 0x00,
+        0x00, 0x00, 0x00, 0x00
+    };
+    
+    return CompReg::WriteKeyValueBin(*Handle, TEXT("Scancode Map"), bin);
+}
